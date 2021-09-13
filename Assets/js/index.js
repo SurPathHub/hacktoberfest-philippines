217f79551d01771775689630103f916eae9bc117
Suzaku <jcfababeir09@gmail.com>
2021-09-13 12:23:25 +0800

    Added: Header Section

---
+++ Assets/js/index.js
@@ -0,0 +1,32 @@
+
+    // The data/time we want to countdown to
+    var countDownDate = new Date("October 1, 2021 01:00:00").getTime();
+
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + "d "
+    document.getElementById("hours").innerHTML = hours + "hr " 
+    document.getElementById("mins").innerHTML = minutes + "min " 
+    document.getElementById("secs").innerHTML = seconds + "sec " 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "The event is started";
+    }
+    }, 1000);
